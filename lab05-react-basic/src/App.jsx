0baeafcedebeab6a97366ddf1127d04852fb3dba
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-05-31 19:54:59 +0700

    feat(react): define class component

---
+++ lab05-react-basic/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Welcome EC Summer course 2022 - FIT@HCMUS" />
+      <content>Content of pages</content>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
