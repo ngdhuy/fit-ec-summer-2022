340d5288f14a919547743a4ae9012fc27546a871
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-05-31 20:44:41 +0700

    feat(react): load data and render list

--- lab05-react-basic/src/components/Content/index.jsx
+++ lab05-react-basic/src/components/Content/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Products from '../Products';
 import './style.css';
 
 function Content(props) {
   return (
     <div className='content'>
       <h1>Content of pages</h1>
+      <hr />
+      <Products />
     </div>
   );
 }
