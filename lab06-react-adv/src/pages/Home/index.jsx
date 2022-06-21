9ccd1e1fdd0cd0f94ad978c1e87028cf9cb5d06f
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-06-21 20:48:57 +0700

    feate(react-adv): create page home

---
+++ lab06-react-adv/src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return(
+    <>
+      <h1>This is Home page</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, accusamus sint dolore consectetur et quasi quae magnam amet pariatur obcaecati at ex error odio, in eum sequi. Quis, veniam magni.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod officiis doloribus officia culpa animi eveniet suscipit? Atque ullam iste ratione cupiditate consectetur nam, maxime, dolores molestiae a alias voluptatem explicabo.</p>
+    </>
+  );
+}
